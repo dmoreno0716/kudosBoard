3477e3e74592bbded1d7d12fae24a950fa18f03b
David Moreno <dmore107@fiu.edu>
2024-06-20 14:49:27 -0700

    migration for backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () =>{
     console.log(`Server is running on http://localhost:${PORT}`)
