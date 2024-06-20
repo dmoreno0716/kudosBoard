5c780376bb68ad31b9c6ab11c75cdb1ce165d710
David Moreno <dmore107@fiu.edu>
2024-06-19 21:53:22 -0700

    pre-installing axios

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; Copyright by Davey :D</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './Header.css';
+
+
+const Header = () => {
+  return (
+    <header className="banner">
+    KudoBoard
+    </header>
+  );
+};
+
+export default Header;
